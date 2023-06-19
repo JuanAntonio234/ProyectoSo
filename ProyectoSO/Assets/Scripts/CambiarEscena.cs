9aea945669b586246ed8ea46a89bd49c5da11b09
unknown <pere.garces@estudiantat.upc.edu>
2023-06-19 20:57:17 +0200

    Version con juego precreado

---
+++ ProyectoSO/Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,34 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public void IrAIniciarSesion()
+    {
+        SceneManager.LoadSceneAsync("IniciarSesion");
+    }
+    public void IrARegistrarse()
+    {
+        SceneManager.LoadSceneAsync("Registrarse");
+    }
+    public void IrAOpciones()
+    {
+        SceneManager.LoadSceneAsync("Opciones");
+    }
+    public void IrAMenu()
+    {
+        SceneManager.LoadSceneAsync("MenuPrincipal");
+    }
+    public void IrAConsulta()
+    {
+        SceneManager.LoadSceneAsync("Consulta");
+    }
+    public void IrAMenuJuego()
+    {
+        SceneManager.LoadSceneAsync("MenuJuego");
+    }
+    public void IrAJuego()
+    {
+        SceneManager.LoadSceneAsync("Juego");
+    }
+}
---
+++ ProyectoSO/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
+    private enum MovementState { idle, running, jumping, falling}
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 13f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && ItsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y  > .1f)
+        {
+            state = MovementState.jumping;
+        }
+
+        if (rb.velocity.y  < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool ItsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+}
